9c7ba4ac4dffe574ea4e451dc377b3f71e45457c
XaviCanoGarcia <canogarciaxavi@gmail.com>
2024-03-10 12:23:12 +0100

    quart commit projecte en blanc JDK 18

--- src/main/java/org/ClientEscriptori/Main.java
+++ src/main/java/org/ClientEscriptori/Main.java
@@ -3,6 +3,6 @@ package org.ClientEscriptori;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("¡Hola, mundo!");
+        System.out.println("¡Hola, numdo!");
     }
 }
