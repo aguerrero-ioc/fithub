a646e70c2818b396b6baffb7bfb3e4a01f61726e
Yogimbo <ivan.fdez.cano@gmail.com>
2024-03-15 18:34:28 +0100

    Resta Test de Login

---
+++ Test/LoginFallitOpcionsExtres.java
@@ -0,0 +1,30 @@
+import org.example.Servidor.consultes.ConsultaLogin;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoginFallitOpcionsExtres {
+    private static final String url = "jdbc:postgresql://localhost:5432/FitHub";
+    static String bbddUsuari = "postgres";
+    static String bbddPass = "ioc";
+    static Connection con;
+
+    @Test
+    public void main() {
+        try {
+            String loginTestFallit = "login,client@fithub.es,passClient,opcioExtra";
+            Class.forName("org.postgresql.Driver");
+
+            con = DriverManager.getConnection(url, bbddUsuari, bbddPass);
+            System.out.println(new ConsultaLogin(con, loginTestFallit).consultaLoginSQL());
+
+            assertEquals("-1", new ConsultaLogin(con, loginTestFallit).consultaLoginSQL());
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
